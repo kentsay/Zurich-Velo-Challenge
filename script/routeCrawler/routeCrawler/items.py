43ee5864e2bdf51e45b53cf42885f06fa586b30d
Shuang Wu <wus@student.ethz.ch>
2015-10-10 18:39:39 +0200

    set default encoding as utf-8; rename RoutecrawlerItem as RouteCrawlerItem

--- script/routeCrawler/routeCrawler/items.py
+++ script/routeCrawler/routeCrawler/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class RoutecrawlerItem(scrapy.Item):
+class RouteCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     distance = scrapy.Field()
--- script/routeCrawler/routeCrawler/spiders/crawler.py
+++ script/routeCrawler/routeCrawler/spiders/crawler.py
@@ -1,36 +1,40 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import scrapy
-from routeCrawler.items import RoutecrawlerItem
+from routeCrawler.items import RouteCrawlerItem
 
-class routeCrawler(scrapy.Spider):
+class RouteCrawler(scrapy.Spider):
     name = "bikemap"
-    allowed_domains = ["bikemap.net"]
+    allowed_domains = "http://www.bikemap.net"
     start_urls = ["http://www.bikemap.net/en/search/?q=zurich"]
 
     def parse(self, response):
-	for result in response.xpath(u'//li[@class="search-result route"]'.encode('utf-8')):
-	    item = RoutecrawlerItem() 
+	for result in response.xpath('//li[@class="search-result route"]'):
+	    item = RouteCrawlerItem() 
 
-	    item['name'] = result.xpath(u'div/h2[@class="search-result-header"]/a/text()'.encode('utf-8')).extract()
+	    item['name'] = result.xpath('div/h2[@class="search-result-header"]/a/text()').extract()[0]
 
-	    item['distance'] = result.xpath(u'div/div/div[@class="search-result__info"]/dl/dd/text()'.encode('utf-8')).re('[0-9]+ km');
+	    item['distance'] = result.xpath('div/div/div[@class="search-result__info"]/dl/dd/text()').re('[0-9]+ km')
 	    if not item['distance']:
+		# Set distance as -1 if it is not provided
 		item['distance'] = -1 
 	    else:
-		item['distance'] = int(item['distance'][0].strip(' km'));
+		item['distance'] = int(item['distance'][0].strip(' km'))
 
-	    item['elevation'] = result.xpath(u'div/div/div[@class="search-result__info"]/dl/dd/text()'.encode('utf-8')).re('[0-9]+ hm');
+	    item['elevation'] = result.xpath('div/div/div[@class="search-result__info"]/dl/dd/text()').re('[0-9]+ hm')
 	    if not item['elevation']:
-		item['elevation'] = -1;
+		# Set elevation as -1 if it is not provided
+		item['elevation'] = -1
 	    else:
-		item['elevation'] = int(item['elevation'][0].strip(' hm'));
+		item['elevation'] = int(item['elevation'][0].strip(' hm'))
 
-	    item['kml_url'] = result.xpath('@data-kml-url').extract()
-	    item['snapshot_url'] = result.xpath(u'div/div/div[@class="search-result__image"]/a/img/@src'.encode('utf-8')).extract()
+	    item['kml_url'] = result.xpath('@data-kml-url').extract()[0]
+	    #TODO: download KML files
 
-	    #print item['name']
-	    #print item['distance']
-	    #print item['elevation']
-	    #print item['kml_url']
+	    item['snapshot_url'] = result.xpath('div/div/div[@class="search-result__image"]/a/img/@src').extract()[0]
+	    item['snapshot_url'] = unicode(self.allowed_domains, 'utf-8') + item['snapshot_url']
+
 	    print item
+
+	    #TODO: store item into database
