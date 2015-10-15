9bd6f30990eb0ecee24ff0d0003b89751cd66748
Shuang Wu <wus@student.ethz.ch>
2015-10-15 22:50:04 +0200

    Save crawled items into SQLite database

--- script/routeCrawler/routeCrawler/spiders/crawler.py
+++ script/routeCrawler/routeCrawler/spiders/crawler.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sqlite3
 import scrapy
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
 from routeCrawler.items import RouteCrawlerItem
 
 class RouteCrawler(scrapy.Spider):
     name = "bikemap"
-    allowed_domains = "http://www.bikemap.net"
-    start_urls = ["http://www.bikemap.net/en/search/?q=zurich"]
+
+    def __init__(self, category='', domain=''):
+	# Connect to spider_closed signal
+	dispatcher.connect(self.spider_closed, signals.spider_closed)
+
+	# Connect to SQLite database
+	self.conn = sqlite3.connect('routes.db')
+	self.c = self.conn.cursor()
+	self.c.execute('''CREATE TABLE if not exists routes (distance, snapshot_url, elevation, name, kml_url)''')
+
+	# Standard Scrapy settings
+	self.allowed_domains = "http://www.bikemap.net"
+	url_prefix = "http://www.bikemap.net/en/regional/Switzerland/Zurich/Zurich/?page="
+	self.start_urls = []
+	# Note: the number of pages is hard-coded
+	for i in range(1, 82):
+	    self.start_urls.append(url_prefix + str(i))
+
+	print "Connected to DB and initialized the crawler"
 
     def parse(self, response):
 	for result in response.xpath('//li[@class="search-result route"]'):
@@ -35,6 +55,13 @@ class RouteCrawler(scrapy.Spider):
 	    item['snapshot_url'] = result.xpath('div/div/div[@class="search-result__image"]/a/img/@src').extract()[0]
 	    item['snapshot_url'] = unicode(self.allowed_domains, 'utf-8') + item['snapshot_url']
 
-	    print item
+	    #print item
 
-	    #TODO: store item into database
+	    # Store the item into database
+	    self.c.execute('INSERT INTO routes VALUES (?,?,?,?,?)', item.values())
+	    self.conn.commit()
+
+    def spider_closed(self, spider):
+	self.conn.close()
+	print "Close DB connection successfully."
+
