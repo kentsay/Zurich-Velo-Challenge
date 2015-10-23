a684a3d1a67bfbb5b52d3cb5a51587f4f5d5d442
Ken Tsay <naturen@gmail.com>
2015-10-23 13:44:49 +0200

    update App Template

---
+++ VeloTemplate/app/src/main/java/ch/ethz/gis/velotemplate/VeloHome.java
@@ -0,0 +1,103 @@
+package ch.ethz.gis.velotemplate;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class VeloHome extends ListActivity {
+
+    VeloRouteAdapter veloAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_velo_home);
+
+        veloAdapter= new VeloRouteAdapter();
+        setListAdapter(veloAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        VeloRoute route = veloAdapter.getVeloRoute(position);
+        Toast.makeText(VeloHome.this, route.route_name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public List<VeloRoute> getDataForListView()
+    {
+        List<VeloRoute> routeList = new ArrayList<VeloRoute>();
+        Random rmd = new Random();
+
+        for(int i=0;i<10;i++)
+        {
+            VeloRoute route = new VeloRoute();
+            route.route_name = "Route " + i;
+            route.route_distance = String.valueOf(rmd.nextInt());
+            routeList.add(route);
+        }
+        return routeList;
+    }
+
+    public class VeloRouteAdapter extends BaseAdapter {
+
+        List<VeloRoute> routeList = getDataForListView();
+        @Override
+        public int getCount() {
+            return routeList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return routeList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return routeList.indexOf(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null)
+            {
+                LayoutInflater inflater = (LayoutInflater) VeloHome.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.route_item, parent,false);
+            }
+
+            TextView routeName = (TextView)convertView.findViewById(R.id.route_name);
+            TextView routeDistance = (TextView)convertView.findViewById(R.id.route_info);
+
+            VeloRoute route = routeList.get(position);
+
+            routeName.setText(route.route_name);
+            routeDistance.setText(route.route_distance);
+
+            return convertView;
+        }
+
+        public VeloRoute getVeloRoute(int position)
+        {
+            return routeList.get(position);
+        }
+
+    }
+}
---
+++ VeloTemplate/app/src/main/java/ch/ethz/gis/velotemplate/VeloRoute.java
@@ -0,0 +1,10 @@
+package ch.ethz.gis.velotemplate;
+
+/**
+ * Created by kentsay on 23/10/2015.
+ */
+public class VeloRoute {
+    String route_name;
+    String route_distance;
+    String route_height;
+}
