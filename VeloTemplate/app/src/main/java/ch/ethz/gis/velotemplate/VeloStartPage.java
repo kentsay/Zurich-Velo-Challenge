9ec3746394dddfd8b45ebcfbaede0942b3834847
Ken Tsay <naturen@gmail.com>
2015-10-30 14:38:40 +0100

    add action bar icon for user to favorit and search

--- VeloTemplate/app/src/main/java/ch/ethz/gis/velotemplate/VeloStartPage.java
+++ VeloTemplate/app/src/main/java/ch/ethz/gis/velotemplate/VeloStartPage.java
@@ -3,6 +3,7 @@ package ch.ethz.gis.velotemplate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -47,5 +48,13 @@ public class VeloStartPage extends Activity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 
 }
