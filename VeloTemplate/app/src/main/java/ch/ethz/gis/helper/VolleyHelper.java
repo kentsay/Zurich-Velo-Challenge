3991beb2648f4e598ff0b5d99126e16466c274f5
Shuang Wu <wus@student.ethz.ch>
2015-11-09 16:27:03 +0100

    import Volley library and update gradle settings

---
+++ VeloTemplate/app/src/main/java/ch/ethz/gis/helper/VolleyHelper.java
@@ -0,0 +1,66 @@
+package ch.ethz.gis.helper;
+
+/**
+ * Created by Shuang Wu on 11/9/15.
+ * Source: https://developer.android.com/training/volley/requestqueue.html#singleton
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+
+public class VolleyHelper {
+    private static VolleyHelper mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private VolleyHelper(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+            private final LruBitmapCache
+                    cache = new LruBitmapCache(20);
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+        });
+    }
+
+    public static synchronized VolleyHelper getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleyHelper(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
